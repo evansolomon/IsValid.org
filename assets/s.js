6c07fc72bdd3736f17ca987defd05050d659b53d
Evan Solomon <evan@evanalyze.com>
2012-09-05 01:25:42 -0700

    Move API requests into their own JS function so we don't need to fake button clicks
    Mildly less hacky

--- assets/s.js
+++ assets/s.js
@@ -3,50 +3,56 @@ function roundNumber(num, dec) {
 	return result;
 }
 
-$(document).ready(function() {
+function submitData(con_con, con_sam, test_con, test_sam, fx) {
+	var dataString = 'conversions_control='+ con_con + '&samples_control=' + con_sam + '&conversions_experiment='+ test_con + '&samples_experiment=' + test_sam + '&function=' + fx;
+
+	$.getJSON("api?" + dataString, function(stat_results){
+		//clear g field
+		$(".column").html("");
+
+		//check for errors
+		if(stat_results.error){
+			return false;
+		}
+
+		//add text input with query args, then autofocus it
+		var permalinkString = dataString.replace("conversions_control","cc").replace("samples_control","sc").replace("conversions_experiment","ce").replace("samples_experiment","se").replace("function","fx");
+		$('<input class="permalink" value="http://'+location.host+location.pathname+'?'+permalinkString+'&permalink=true" type="text">').prependTo("#action").focus();
+			$("input[type=text].permalink").focus(function(){
+				$(this).select();
+			})
+			.mouseup(function(e){
+				e.preventDefault();
+			});
+
+		//insert confidence charts
+		var control_confidence = "<div class='chart confidence'><img src='"+stat_results.confidence.chart.control+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.confidence.results.control.low * 100,1)+" - "+roundNumber(stat_results.confidence.results.control.high * 100,1)+"%</div><div class='cat'>Control performance</div></div>";
+		var experiment_confidence = "<div class='chart confidence'><img src='"+stat_results.confidence.chart.experiment+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.confidence.results.experiment.low * 100,1)+" - "+roundNumber(stat_results.confidence.results.experiment.high * 100,1)+"%</div><div class='cat'>Test performance</div></div>";
+
+		$(control_confidence + experiment_confidence).appendTo(".column.left");
+
+		//insert significance and improvement charts
+		var significance = "<div class='chart significance'><img src='"+stat_results.significance.chart+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.significance.results.experiment * 100,1)+"%</div><div class='cat'>Chance of outperformance</div></div>";
+		var improvement = "<div class='chart improvement'><img src='"+stat_results.improvement.chart+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.improvement.results.low * 100,1)+" - "+roundNumber(stat_results.improvement.results.high * 100,1)+"%</div><div class='cat'>Likely improvement</div></div>";
+
+		$(significance + improvement).appendTo(".column.right");
+	});
+
+	return false;
+}
+
+$(function() {
 	$("#con_con").focus();
 
-	$(".button").click(function() {
-		var con_con = $("input#con_con").val();
-		var con_sam = $("input#con_sam").val();
-		var test_con = $("input#test_con").val();
-		var test_sam = $("input#test_sam").val();
-		var fx = $("input#fx").val();
-		var dataString = 'conversions_control='+ con_con + '&samples_control=' + con_sam + '&conversions_experiment='+ test_con + '&samples_experiment=' + test_sam + '&function=' + fx;
-
-		$.getJSON("api?" + dataString, function(stat_results){
-			//clear g field
-			$(".column").html("");
-
-			//check for errors
-			if(stat_results.error){
-				return false;
-			}
-
-			//add text input with query args, then autofocus it
-			var permalinkString = dataString.replace("conversions_control","cc").replace("samples_control","sc").replace("conversions_experiment","ce").replace("samples_experiment","se").replace("function","fx");
-			$('<input class="permalink" value="http://'+location.host+location.pathname+'?'+permalinkString+'&permalink=true" type="text">').prependTo("#action").focus();
-				$("input[type=text].permalink").focus(function(){
-					$(this).select();
-				})
-				.mouseup(function(e){
-					e.preventDefault();
-				});
-
-			//insert confidence charts
-			var control_confidence = "<div class='chart confidence'><img src='"+stat_results.confidence.chart.control+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.confidence.results.control.low * 100,1)+" - "+roundNumber(stat_results.confidence.results.control.high * 100,1)+"%</div><div class='cat'>Control performance</div></div>";
-			var experiment_confidence = "<div class='chart confidence'><img src='"+stat_results.confidence.chart.experiment+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.confidence.results.experiment.low * 100,1)+" - "+roundNumber(stat_results.confidence.results.experiment.high * 100,1)+"%</div><div class='cat'>Test performance</div></div>";
-
-			$(control_confidence + experiment_confidence).appendTo(".column.left");
-
-			//insert significance and improvement charts
-			var significance = "<div class='chart significance'><img src='"+stat_results.significance.chart+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.significance.results.experiment * 100,1)+"%</div><div class='cat'>Chance of outperformance</div></div>";
-			var improvement = "<div class='chart improvement'><img src='"+stat_results.improvement.chart+"' class='chart-image' /><div class='num'>"+roundNumber(stat_results.improvement.results.low * 100,1)+" - "+roundNumber(stat_results.improvement.results.high * 100,1)+"%</div><div class='cat'>Likely improvement</div></div>";
-
-			$(significance + improvement).appendTo(".column.right")
-		});
-
-		return false;
+	$("form").on('submit', function(event){
+		event.preventDefault();
+		submitData(
+			$("input#con_con").val(),
+			$("input#con_sam").val(),
+			$("input#test_con").val(),
+			$("input#test_sam").val(),
+			$("input#fx").val()
+		);
 	});
 
 	//get results on permalink pages
@@ -68,6 +74,12 @@ $(document).ready(function() {
 		$("input#con_sam").val(getParameter("sc"));
 		$("input#test_con").val(getParameter("ce"));
 		$("input#test_sam").val(getParameter("se"));
-		$(".button").click();
+		submitData(
+			getParameter("cc"),
+			getParameter("sc"),
+			getParameter("ce"),
+			getParameter("se"),
+			getParameter("fx")
+		);
 	}
 });
