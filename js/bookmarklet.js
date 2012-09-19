0256cd16860e78213957d633c092cba551fe5157
Evan Solomon <evan@evanalyze.com>
2012-09-18 22:11:11 -0700

    Make bookmarklet reusable
    * Cleanup after usage is complete (remove UI changes, reset vars)
    * Call reset() at the beginning of init() in case the bookmarklet is fired twice
    * Move hover callbacks to separate functions so they can be unbound

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,16 +1,14 @@
 (function(){
 	// Load jQuery if it isn't here
 	if ( window.jQuery === undefined ) {
-		var done = false,
-			script = document.createElement("script");
+		var script = document.createElement("script");
 
 		script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js";
 		script.onload = script.onreadystatechange = function() {
-			if ( ! done && ( ! this.readyState || this.readyState == "loaded" || this.readyState == 'complete' ) ) {
-				done = true;
+			if ( ! this.readyState || this.readyState == "loaded" || this.readyState == 'complete' )
 				init();
-			}
 		};
+
 		document.getElementsByTagName('head')[0].appendChild( script );
 	}
 	else {
@@ -18,14 +16,13 @@
 	}
 
 	function fancyClickThing( event ) {
-		var text = event.target.innerHTML;
-		var number = parseInt( text.replace( ',', '' ), 10 );
+		var text = event.target.innerHTML,
+			number = parseInt( text.replace( ',', '' ), 10 );
+
 		window.isvalidClicks.push( number );
 		jQuery(event.target).css( 'background-color', 'rgba(50, 200, 80, 0.7)' ).addClass( 'isvalid-clicked' );
 
 		if ( 4 == window.isvalidClicks.length ) {
-			jQuery(this).unbind( 'mouseup', fancyClickThing );
-
 			window.open( 'http://isvalid.org/' +
 				'?sc=' + window.isvalidClicks[0] +
 				'&cc=' + window.isvalidClicks[1] +
@@ -37,37 +34,40 @@
 		}
 	}
 
+	function fancyMouseEnter( event ) {
+		var element = jQuery(this);
+		if ( element.hasClass( 'isvalid-clicked' ) )
+			return;
+
+		event.stopPropagation();
+		var text = event.target.innerHTML,
+			number = parseInt( text.replace( ',', '' ), 10 );
+
+		if ( ! isNaN( parseFloat( number ) ) && isFinite( number ) )
+			element.css( 'background-color', 'rgba(50, 200, 80, 0.25)' );
+	}
+
+	function fancyMouseLeave( event ) {
+		var element = jQuery(this);
+		if ( ! element.hasClass( 'isvalid-clicked' ) )
+			element.css( 'background-color', '' );
+	}
+
 	function init() {
+		reset(); // In case it gets run twice in a row
+
 		window.isvalidClicks = [];
-		window.isvalidBookmarklet = (function() {
+		(function() {
 			jQuery( document ).mouseup( fancyClickThing );
+			jQuery('*').hover( fancyMouseEnter, fancyMouseLeave );
 		})();
-
-		jQuery('*').hover(
-			function( event ) {
-				var element = jQuery(this);
-				if ( element.hasClass( 'isvalid-clicked' ) )
-					return;
-
-				event.stopPropagation();
-				var text = event.target.innerHTML;
-				var number = parseInt( text.replace( ',', '' ), 10 );
-				if ( ! isNaN( parseFloat( number ) ) && isFinite( number ) )
-					element.css( 'background-color', 'rgba(50, 200, 80, 0.25)' );
-			},
-
-			function( event ) {
-				event.stopPropagation();
-				var element = jQuery(this);
-				if ( ! element.hasClass( 'isvalid-clicked' ) )
-					element.css( 'background-color', '' );
-			}
-		);
-
 	}
 
 	function reset() {
 		jQuery('.isvalid-clicked').removeClass( 'isvalid-clicked' ).css( 'background-color', '' );
+		jQuery(document).unbind( 'mouseup', fancyClickThing );
+		jQuery('*').unbind( 'mouseleave', fancyMouseLeave ).unbind( 'mouseenter', fancyMouseEnter );
+
 		window.isvalidClicks = [];
 	}
 })();
