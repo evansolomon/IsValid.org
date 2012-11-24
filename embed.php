60a7d38a46a81904b823c3845beff4ac26cdd9aa
Evan Solomon <evan@evanalyze.com>
2012-11-23 17:30:52 -0800

    Use header template on index and embed pages

--- embed.php
+++ embed.php
@@ -1,33 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-		<title>IsValid | Quantify the results of A/B tests</title>
-
-		<!--[if lt IE 9]>
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-
-		<!-- Libraries -->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/underscore.min.js"></script>
-		<script src="js/handlebars.js"></script>
-
-		<script src="js/isvalid.js"></script>
-
-		<!-- Fonts -->
-		<script type="text/javascript" src="//use.typekit.net/ivm8epx.js"></script>
-		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-
-		<!-- Bootstrap -->
-		<link href="css/bootstrap-compiled.min.css" rel="stylesheet">
-
-		<link href="css/isvalid.css" rel="stylesheet">
-		<link href="css/embed.css" rel="stylesheet">
-	</head>
-
+<?php print_header( true ); ?>
 	<body class="embed">
 		<script id="results-template" type="text/x-handlebars-template">
 			<div class="row">
