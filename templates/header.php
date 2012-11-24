6473f8fb9c083abf875a76540958be894e582a6c
Evan Solomon <evan@evanalyze.com>
2012-11-23 17:30:20 -0800

    Templatize header

---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<?php
+
+function print_header( $embed = false ) {
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title>IsValid | Quantify the results of A/B tests</title>
+
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Libraries -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/underscore.min.js"></script>
+		<script src="js/handlebars.js"></script>
+
+		<script src="js/isvalid.js"></script>
+
+		<!-- Fonts -->
+		<script type="text/javascript" src="//use.typekit.net/ivm8epx.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap-compiled.min.css" rel="stylesheet">
+
+		<link href="css/isvalid.css" rel="stylesheet">
+	<?php if ( $embed ): ?>
+		<link href="css/embed.css" rel="stylesheet">
+	<?php endif; ?>
+</head>
+}
