759ea953a006374e164b1600d1f1796f8b88d589
Evan Solomon <evan@evanalyze.com>
2012-11-30 12:47:56 -0800

    Move footer to a template

--- isvalid-load.php
+++ isvalid-load.php
@@ -4,3 +4,4 @@
 
 require_once( dirname( __FILE__ ) . '/templates/header.php' );
 require_once( dirname( __FILE__ ) . '/templates/handlebars.php' );
+require_once( dirname( __FILE__ ) . '/templates/footer.php' );
