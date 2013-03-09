54ccdc0e916ca5497914c2d6d6ac1109da75548d
Evan Solomon <evan@evanalyze.com>
2013-03-09 05:29:21 -0800

    Remove Handlebars PHP template from embeds

--- embed.php
+++ embed.php
@@ -1,8 +1,6 @@
 <?php require_once( dirname( __FILE__ ) . '/isvalid-load.php' ); ?>
 <?php print_header( true ); ?>
 	<body class="embed">
-		<?php Handlebars::print_templates(); ?>
-
 		<div class="container">
 			<div class="results"></div>
 		</div> <!-- /container -->
