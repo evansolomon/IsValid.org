e5ea1d5948af25a99221eb3712b44bec6fb660fe
Evan Solomon <evan@evanalyze.com>
2013-03-09 05:26:30 -0800

    Don't load Handlebars PHP template anymore, it doesn't exist

--- isvalid-load.php
+++ isvalid-load.php
@@ -3,5 +3,4 @@
 // Centralize loading helper functions
 
 require_once( dirname( __FILE__ ) . '/templates/header.php' );
-require_once( dirname( __FILE__ ) . '/templates/handlebars.php' );
 require_once( dirname( __FILE__ ) . '/templates/footer.php' );
